29a3d7adb6965bb54d6a6dd6ddadd5330bd0749b
Tormod Fossum <tormod.fossum@nav.no>
2023-10-18 15:04:33 +0200

    Legg til oppsett for kafkakonsumere

---
+++ src/main/kotlin/no/nav/amt/deltaker/bff/kafka/ManagedKafkaConsumer.kt
@@ -0,0 +1,58 @@
+package no.nav.amt.deltaker.bff.kafka
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.apache.kafka.clients.consumer.KafkaConsumer
+import org.slf4j.LoggerFactory
+import java.time.Duration
+
+class ManagedKafkaConsumer<K, V>(
+    val topic: String,
+    val config: Map<String, *>,
+    val consume: (key: K, value: V) -> Unit,
+) {
+    private val log = LoggerFactory.getLogger(javaClass)
+    private val job = Job()
+    private val scope = CoroutineScope(Dispatchers.Default + job)
+
+    private var running = true
+
+    fun run() = scope.launch {
+        KafkaConsumer<K, V>(config).use { consumer ->
+            consumer.subscribe(listOf(topic))
+            while (running) {
+                consumer.poll(Duration.ofMillis(1000)).forEach { record ->
+                    process(record)
+                }
+                consumer.commitSync()
+            }
+        }
+    }
+
+    private suspend fun process(record: ConsumerRecord<K, V>) {
+        var retries = 0
+        var success = false
+
+        while (!success) {
+            try {
+                consume(record.key(), record.value())
+                log.debug("Consumed record topic=${record.topic()} partition=${record.partition()} offset=${record.offset()}")
+                success = true
+            } catch (t: Throwable) {
+                log.error("Failed to consume record topic=${record.topic()} partition=${record.partition()} offset=${record.offset()} error=$t")
+                delay(exponentialBackoff(++retries))
+            }
+        }
+    }
+
+    fun stop() {
+        running = false
+        job.cancel()
+    }
+
+    private fun exponentialBackoff(retries: Int) = 1000L * (retries * retries)
+}
---
+++ src/test/kotlin/no/nav/amt/deltaker/bff/kafka/ManagedKafkaConsumerTest.kt
@@ -0,0 +1,119 @@
+package no.nav.amt.deltaker.bff.kafka
+
+import io.kotest.matchers.shouldBe
+import no.nav.amt.deltaker.bff.kafka.config.LocalKafkaConfig
+import no.nav.amt.deltaker.bff.utils.AsyncUtils
+import org.apache.kafka.clients.producer.KafkaProducer
+import org.apache.kafka.clients.producer.ProducerConfig
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.clients.producer.RecordMetadata
+import org.apache.kafka.common.serialization.ByteArrayDeserializer
+import org.apache.kafka.common.serialization.ByteArraySerializer
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.kafka.common.serialization.StringSerializer
+import org.apache.kafka.common.serialization.UUIDDeserializer
+import org.apache.kafka.common.serialization.UUIDSerializer
+import org.junit.Test
+import java.util.Properties
+import java.util.UUID
+
+class ManagedKafkaConsumerTest {
+    val topic = "test.topic"
+
+    val stringConsumerConfig = LocalKafkaConfig(SingletonKafkaProvider.getHost()).consumerConfig(
+        keyDeserializer = StringDeserializer(),
+        valueDeserializer = StringDeserializer(),
+        groupId = "test-consumer",
+    )
+
+    @Test
+    fun `ManagedKafkaConsumer - konsumerer record med String, String`() {
+        val key = "key"
+        val value = "value"
+        val cache = mutableMapOf<String, String>()
+
+        produce(ProducerRecord(topic, key, value))
+
+        val consumer = ManagedKafkaConsumer(topic, stringConsumerConfig) { k: String, v: String ->
+            cache[k] = v
+        }
+        consumer.run()
+
+        AsyncUtils.eventually {
+            cache[key] shouldBe value
+            consumer.stop()
+        }
+    }
+
+    @Test
+    fun `ManagedKafkaConsumer - konsumerer record med UUID, ByteArray`() {
+        val key = UUID.randomUUID()
+        val value = "value".toByteArray()
+        val cache = mutableMapOf<UUID, ByteArray>()
+        val uuidTopic = "uuid.topic"
+
+        produceUUIDByteArray(ProducerRecord(uuidTopic, key, value))
+
+        val config = LocalKafkaConfig(SingletonKafkaProvider.getHost())
+            .consumerConfig(
+                keyDeserializer = UUIDDeserializer(),
+                valueDeserializer = ByteArrayDeserializer(),
+                groupId = "test-consumer",
+            )
+
+        val consumer = ManagedKafkaConsumer(uuidTopic, config) { k: UUID, v: ByteArray ->
+            cache[k] = v
+        }
+        consumer.run()
+
+        AsyncUtils.eventually {
+            cache[key] shouldBe value
+            consumer.stop()
+        }
+    }
+
+    @Test
+    fun `ManagedKafkaConsumer - prøver å konsumere melding på nytt hvis noe feiler`() {
+        val key = "key"
+        val value = "value"
+
+        var antallGangerKallt = 0
+
+        produce(ProducerRecord(topic, key, value))
+
+        val consumer = ManagedKafkaConsumer<String, String>(topic, stringConsumerConfig) { _, _ ->
+            antallGangerKallt++
+            throw IllegalStateException("skal feile noen ganger")
+        }
+        consumer.run()
+
+        AsyncUtils.eventually {
+            antallGangerKallt shouldBe 2
+            consumer.stop()
+        }
+    }
+
+    private fun produce(record: ProducerRecord<String, String>): RecordMetadata {
+        KafkaProducer<String, String>(
+            Properties().apply {
+                put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, SingletonKafkaProvider.getHost())
+                put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer::class.java)
+                put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer::class.java)
+            },
+        ).use { producer ->
+            return producer.send(record).get()
+        }
+    }
+
+    private fun produceUUIDByteArray(record: ProducerRecord<UUID, ByteArray>): RecordMetadata {
+        KafkaProducer<UUID, ByteArray>(
+            Properties().apply {
+                put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, SingletonKafkaProvider.getHost())
+                put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, UUIDSerializer::class.java)
+                put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, ByteArraySerializer::class.java)
+            },
+        ).use { producer ->
+            return producer.send(record).get()
+        }
+    }
+}
