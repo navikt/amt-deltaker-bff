87e2bac252a68112ad138f492236aa71e94675ba
Tormod Fossum <110398791+tofoss@users.noreply.github.com>
2023-10-24 13:31:01 +0200

    Bruk default verdi i steden for miljøvariabel som ikke finnes (#15)

--- src/main/kotlin/no/nav/amt/deltaker/bff/kafka/config/KafkaConfigImpl.kt
+++ src/main/kotlin/no/nav/amt/deltaker/bff/kafka/config/KafkaConfigImpl.kt
@@ -1,7 +1,6 @@
 package no.nav.amt.deltaker.bff.kafka.config
 
 import no.nav.amt.deltaker.bff.getEnvVar
-import org.apache.kafka.clients.CommonClientConfigs
 import org.apache.kafka.clients.CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG
 import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.clients.producer.ProducerConfig
@@ -10,12 +9,11 @@ import org.apache.kafka.common.serialization.Deserializer
 import org.apache.kafka.common.serialization.StringSerializer
 
 class KafkaConfigImpl : KafkaConfig {
-    private val JAVA_KEYSTORE = "JKS"
-    private val PKCS12 = "PKCS12"
+    private val javaKey = "JKS"
+    private val pkcs12 = "PKCS12"
     private val autoOffsetReset: String = "latest"
 
     private val kafkaBrokers = getEnvVar("KAFKA_BROKERS")
-    private val kafkaSecurityProtocol = getEnvVar("KAFKA_SECURITY_PROTOCOL:SSL")
     private val kafkaTruststorePath = getEnvVar("KAFKA_TRUSTSTORE_PATH")
     private val kafkaCredstorePassword = getEnvVar("KAFKA_CREDSTORE_PASSWORD")
     private val kafkaKeystorePath = getEnvVar("KAFKA_KEYSTORE_PATH")
@@ -27,10 +25,9 @@ class KafkaConfigImpl : KafkaConfig {
     ) + securityConfig()
 
     private fun securityConfig() = mapOf(
-        CommonClientConfigs.SECURITY_PROTOCOL_CONFIG to kafkaSecurityProtocol,
         SslConfigs.SSL_ENDPOINT_IDENTIFICATION_ALGORITHM_CONFIG to "", // Disable server host name verification
-        SslConfigs.SSL_TRUSTSTORE_TYPE_CONFIG to JAVA_KEYSTORE,
-        SslConfigs.SSL_KEYSTORE_TYPE_CONFIG to PKCS12,
+        SslConfigs.SSL_TRUSTSTORE_TYPE_CONFIG to javaKey,
+        SslConfigs.SSL_KEYSTORE_TYPE_CONFIG to pkcs12,
         SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG to kafkaTruststorePath,
         SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG to kafkaCredstorePassword,
         SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG to kafkaKeystorePath,
