369112f53f8f19ab5dd24df7a91396564d1be64e
Tia Firing <tia.firing@nav.no>
2023-12-13 14:39:34 +0100

    fjerner testlogging

--- src/main/kotlin/no/nav/amt/deltaker/bff/auth/TilgangskontrollService.kt
+++ src/main/kotlin/no/nav/amt/deltaker/bff/auth/TilgangskontrollService.kt
@@ -1,33 +1,24 @@
 package no.nav.amt.deltaker.bff.auth
 
-import no.nav.amt.deltaker.bff.application.plugins.objectMapper
 import no.nav.poao_tilgang.client.Decision
 import no.nav.poao_tilgang.client.NavAnsattTilgangTilEksternBrukerPolicyInput
 import no.nav.poao_tilgang.client.PoaoTilgangCachedClient
 import no.nav.poao_tilgang.client.TilgangType
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 import java.util.UUID
 
 class TilgangskontrollService(
     private val poaoTilgangCachedClient: PoaoTilgangCachedClient,
 ) {
-    val log: Logger = LoggerFactory.getLogger(javaClass)
-
     fun verifiserSkrivetilgang(navAnsattAzureId: UUID, norskIdent: String) {
-        log.info("Sjekker tilgang for ansattid $navAnsattAzureId")
-        val tilgangResult = poaoTilgangCachedClient.evaluatePolicy(
+        val tilgang = poaoTilgangCachedClient.evaluatePolicy(
             NavAnsattTilgangTilEksternBrukerPolicyInput(
                 navAnsattAzureId,
                 TilgangType.SKRIVE,
                 norskIdent,
             ),
-        ).get()
-        log.info("Resultat: ${objectMapper.writeValueAsString(tilgangResult)}")
-        val tilgang = tilgangResult ?: throw RuntimeException("Resultat mangler")
+        ).getOrDefault(Decision.Deny("Ansatt har ikke skrivetilgang til bruker", ""))
 
         if (tilgang.isDeny) {
-            log.error("Ikke skrivetilgang")
             throw AuthorizationException("Ansatt har ikke skrivetilgang til bruker")
         }
     }
