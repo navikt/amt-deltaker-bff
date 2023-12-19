9b4987e4c0c6ba0386469b761bf080fd9f890a80
Tia Firing <tia.firing@nav.no>
2023-12-19 13:59:22 +0100

    mer informasjon om feil

--- src/main/kotlin/no/nav/amt/deltaker/bff/db/Database.kt
+++ src/main/kotlin/no/nav/amt/deltaker/bff/db/Database.kt
@@ -46,7 +46,7 @@ object Database {
             .connectRetries(5)
             .dataSource(dataSource)
             .initSql(initSql)
-            .locations("classpath:/db/migration")
+            .validateMigrationNaming(true)
             .load()
             .migrate()
             .migrations
