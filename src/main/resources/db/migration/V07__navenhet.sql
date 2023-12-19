b37cb976895b13496a1774e110e3d146d0cab87b
Tia Firing <tia.firing@nav.no>
2023-12-19 12:55:55 +0100

    prøver å få flywayscript til å kjøre

--- src/main/resources/db/migration/V07__navenhet.sql
+++ src/main/resources/db/migration/V07__navenhet.sql
@@ -4,7 +4,8 @@ create table nav_enhet
     nav_enhet_nummer varchar                  not null,
     navn             varchar                  not null,
     created_at       timestamp with time zone not null default current_timestamp,
-    modified_at      timestamp with time zone not null default current_timestamp
+    modified_at      timestamp with time zone not null default current_timestamp,
+    unique (nav_enhet_nummer)
 );
 
 create index nav_enhet_nav_enhet_nr_idx on nav_enhet (nav_enhet_nummer);
