7dca2cebe4915ccbc94d58d47b729b57a5786847
Tormod Fossum <tormod.fossum@nav.no>
2024-01-08 15:21:23 +0100

    Fiks test

--- src/test/kotlin/no/nav/amt/deltaker/bff/deltaker/DeltakerHistorikkServiceTest.kt
+++ src/test/kotlin/no/nav/amt/deltaker/bff/deltaker/DeltakerHistorikkServiceTest.kt
@@ -3,10 +3,12 @@ package no.nav.amt.deltaker.bff.deltaker
 import io.kotest.matchers.shouldBe
 import no.nav.amt.deltaker.bff.deltaker.db.DeltakerEndringRepository
 import no.nav.amt.deltaker.bff.deltaker.db.DeltakerSamtykkeRepository
+import no.nav.amt.deltaker.bff.deltaker.db.sammenlignDeltakere
 import no.nav.amt.deltaker.bff.deltaker.model.DeltakerHistorikk
 import no.nav.amt.deltaker.bff.utils.SingletonPostgresContainer
 import no.nav.amt.deltaker.bff.utils.data.TestData
 import no.nav.amt.deltaker.bff.utils.data.TestRepository
+import no.nav.amt.deltaker.bff.utils.shouldBeCloseTo
 import org.junit.BeforeClass
 import org.junit.Test
 import java.time.LocalDateTime
@@ -48,9 +50,9 @@ class DeltakerHistorikkServiceTest {
         val historikk = service.getForDeltaker(deltaker.id)
 
         historikk.size shouldBe 3
-        historikk[0] shouldBe DeltakerHistorikk.Endring(nyEndring)
-        historikk[1] shouldBe DeltakerHistorikk.Endring(gammelEndring)
-        historikk[2] shouldBe DeltakerHistorikk.Samtykke(samtykke)
+        sammenlignHistorikk(historikk[0], DeltakerHistorikk.Endring(nyEndring))
+        sammenlignHistorikk(historikk[1], DeltakerHistorikk.Endring(gammelEndring))
+        sammenlignHistorikk(historikk[2], DeltakerHistorikk.Samtykke(samtykke))
     }
 
     @Test
@@ -59,5 +61,31 @@ class DeltakerHistorikkServiceTest {
         TestRepository.insert(deltaker)
 
         service.getForDeltaker(deltaker.id) shouldBe emptyList()
+    }
+}
+
+fun sammenlignHistorikk(a: DeltakerHistorikk, b: DeltakerHistorikk) {
+    when (a) {
+        is DeltakerHistorikk.Endring -> {
+            b as DeltakerHistorikk.Endring
+            a.endring.id shouldBe b.endring.id
+            a.endring.endringstype shouldBe b.endring.endringstype
+            a.endring.endring shouldBe b.endring.endring
+            a.endring.endretAv shouldBe b.endring.endretAv
+            a.endring.endretAvEnhet shouldBe b.endring.endretAvEnhet
+            a.endring.endret shouldBeCloseTo b.endring.endret
+        }
+
+        is DeltakerHistorikk.Samtykke -> {
+            b as DeltakerHistorikk.Samtykke
+            a.samtykke.id shouldBe b.samtykke.id
+            a.samtykke.deltakerId shouldBe b.samtykke.deltakerId
+            a.samtykke.godkjent shouldBeCloseTo b.samtykke.godkjent
+            a.samtykke.gyldigTil shouldBeCloseTo b.samtykke.gyldigTil
+            sammenlignDeltakere(a.samtykke.deltakerVedSamtykke, b.samtykke.deltakerVedSamtykke)
+            a.samtykke.opprettetAv shouldBe b.samtykke.opprettetAv
+            a.samtykke.opprettetAvEnhet shouldBe b.samtykke.opprettetAvEnhet
+            a.samtykke.opprettet shouldBeCloseTo b.samtykke.opprettet
+        }
     }
 }
