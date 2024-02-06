7c81ae52ad38101b5d5dc21ce7cc2f223ef28177
Line Moseng <line.moseng@nav.no>
2024-02-06 14:18:31 +0100

    Tester for avbryt-metode i service

--- src/test/kotlin/no/nav/amt/deltaker/bff/deltaker/PameldingServiceTest.kt
+++ src/test/kotlin/no/nav/amt/deltaker/bff/deltaker/PameldingServiceTest.kt
@@ -376,4 +376,19 @@ class PameldingServiceTest {
             }
         }
     }
+
+    @Test
+    fun `avbrytUtkast - har ikke status utkast til paamelding - kaster feil`() {
+        val deltaker = TestData.lagDeltaker(status = TestData.lagDeltakerStatus(type = DeltakerStatus.Type.VURDERES))
+        val opprettetAv = TestData.randomNavIdent()
+        val opprettetAvEnhet = TestData.randomEnhetsnummer()
+        val aarsak = DeltakerStatus.Aarsak(DeltakerStatus.Aarsak.Type.ANNET, "Flyttet til Spania")
+        TestRepository.insert(deltaker)
+
+        runBlocking {
+            assertFailsWith<IllegalArgumentException> {
+                pameldingService.avbrytUtkast(deltaker, opprettetAvEnhet, opprettetAv, aarsak)
+            }
+        }
+    }
 }
