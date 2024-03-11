f541a88b87db2c130a1d1a967e7de5851bf8a456
Tormod Fossum <tormod.fossum@nav.no>
2024-03-11 12:30:34 +0100

    Oppdater databasse med response fra deltakerendringen

--- src/test/kotlin/no/nav/amt/deltaker/bff/deltaker/DeltakerServiceTest.kt
+++ src/test/kotlin/no/nav/amt/deltaker/bff/deltaker/DeltakerServiceTest.kt
@@ -8,6 +8,7 @@ import no.nav.amt.deltaker.bff.deltaker.model.Innhold
 import no.nav.amt.deltaker.bff.utils.MockResponseHandler
 import no.nav.amt.deltaker.bff.utils.SingletonPostgresContainer
 import no.nav.amt.deltaker.bff.utils.data.TestData
+import no.nav.amt.deltaker.bff.utils.data.TestRepository
 import no.nav.amt.deltaker.bff.utils.data.endre
 import no.nav.amt.deltaker.bff.utils.mockAmtDeltakerClient
 import org.junit.Test
@@ -21,8 +22,10 @@ class DeltakerServiceTest {
     private val service = DeltakerService(DeltakerRepository(), mockAmtDeltakerClient())
 
     @Test
-    fun `oppdaterDeltaker - kaller client og returnerer dum deltaker`(): Unit = runBlocking {
+    fun `oppdaterDeltaker - kaller client og returnerer deltaker`(): Unit = runBlocking {
         val deltaker = TestData.lagDeltaker()
+        TestRepository.insert(deltaker)
+
         val endringer = listOf(
             DeltakerEndring.Endring.EndreBakgrunnsinformasjon("foo"),
             DeltakerEndring.Endring.EndreInnhold(listOf(Innhold("tekst,", "innholdskode,", true, "beskrivelse"))),
