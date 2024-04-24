315b17d6e31749f2a9d02a10cad34215ccff2d28
Tormod Fossum <tormod.fossum@nav.no>
2024-04-24 10:22:48 +0200

    Fjern default ukjent tiltakskode

--- src/main/kotlin/no/nav/amt/deltaker/bff/deltakerliste/tiltakstype/Tiltakstype.kt
+++ src/main/kotlin/no/nav/amt/deltaker/bff/deltakerliste/tiltakstype/Tiltakstype.kt
@@ -34,7 +34,6 @@ data class Tiltakstype(
         JOBBKLUBB,
         OPPFOLGING,
         VARIG_TILRETTELAGT_ARBEID_SKJERMET,
-        UKJENT, // midlertidig kode til alle har blitt lest inn;
     }
 }
 
