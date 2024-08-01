342bcf39919f0275bf3be77c01db1e083bf47de9
Tormod Fossum <tormod.fossum@nav.no>
2024-08-01 16:26:36 +0200

    Bump amt-lib

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,7 +36,7 @@ val postgresVersion = "42.7.3"
 val caffeineVersion = "3.1.8"
 val mockkVersion = "1.13.12"
 val nimbusVersion = "9.40"
-val amtLibVersion = "1.2024.07.23_10.10-3d9393ed169a"
+val amtLibVersion = "1.2024.08.01_14.19-25cd695f0dd1"
 
 dependencies {
     implementation("io.ktor:ktor-server-content-negotiation-jvm")
