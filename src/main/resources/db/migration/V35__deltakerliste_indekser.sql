93443f83994c1ddb1a72799b3836e7ded7633bf2
Tia Firing <tia.firing@nav.no>
2024-10-03 11:20:21 +0200

    legger til manglende indekser

---
+++ src/main/resources/db/migration/V35__deltakerliste_indekser.sql
@@ -0,0 +1,3 @@
+create index deltakerliste_arrangor_id_idx on deltakerliste (arrangor_id);
+
+create index deltakerliste_tiltakstype_id_idx on deltakerliste (tiltakstype_id);
---
+++ src/main/resources/db/migration/V37__deltaker_indekser.sql
@@ -0,0 +1,3 @@
+create index deltaker_person_id_idx on deltaker (person_id);
+
+create index deltaker_deltakerliste_id_idx on deltaker (deltakerliste_id);
