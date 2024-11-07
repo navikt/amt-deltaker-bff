81c54b61f40c660dc65f202737bcd0671bd0cb45
tiafiring <tiafiring@gmail.com>
2024-11-07 06:57:40 +0100

    skal kunne endre fra ikke aktuell til ikke aktuell så lenge det er en annen årsak (#370)

--- src/main/kotlin/no/nav/amt/deltaker/bff/deltaker/api/model/IkkeAktuellRequest.kt
+++ src/main/kotlin/no/nav/amt/deltaker/bff/deltaker/api/model/IkkeAktuellRequest.kt
@@ -1,7 +1,9 @@
 package no.nav.amt.deltaker.bff.deltaker.api.model
 
+import no.nav.amt.deltaker.bff.deltaker.api.utils.harEndretSluttaarsak
 import no.nav.amt.deltaker.bff.deltaker.api.utils.validerAarsaksBeskrivelse
 import no.nav.amt.deltaker.bff.deltaker.api.utils.validerBegrunnelse
+import no.nav.amt.deltaker.bff.deltaker.api.utils.validerDeltakerKanEndres
 import no.nav.amt.deltaker.bff.deltaker.model.Deltaker
 import no.nav.amt.lib.models.deltaker.DeltakerEndring
 import no.nav.amt.lib.models.deltaker.DeltakerStatus
@@ -12,11 +14,22 @@ data class IkkeAktuellRequest(
     val begrunnelse: String?,
     override val forslagId: UUID?,
 ) : EndringsforslagRequest {
+    private val kanBliIkkeAktuell = listOf(DeltakerStatus.Type.VENTER_PA_OPPSTART, DeltakerStatus.Type.IKKE_AKTUELL)
+
     override fun valider(deltaker: Deltaker) {
         validerAarsaksBeskrivelse(aarsak.beskrivelse)
-        require(deltaker.status.type == DeltakerStatus.Type.VENTER_PA_OPPSTART) {
+        require(deltaker.status.type in kanBliIkkeAktuell) {
             "Kan ikke sette deltaker med status ${deltaker.status.type} til ikke aktuell"
         }
+        validerDeltakerKanEndres(deltaker)
         validerBegrunnelse(begrunnelse)
+        require(deltakerErEndret(deltaker)) {
+            "Kan ikke oppdatere deltaker som allerede er ikke aktuell med samme årsak"
+        }
+    }
+
+    private fun deltakerErEndret(deltaker: Deltaker): Boolean {
+        return deltaker.status.type != DeltakerStatus.Type.IKKE_AKTUELL ||
+            harEndretSluttaarsak(deltaker.status.aarsak, aarsak)
     }
 }
