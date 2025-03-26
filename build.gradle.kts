0a1b0aad23a46538f6bbfd4bd7141efb89c1e030
Tormod Fossum <110398791+tofoss@users.noreply.github.com>
2025-03-26 09:19:34 +0100

    Bump dependencies - 20250326090858 (#502)
    * Bump kotlinVersion from 2.1.10 to 2.1.20
    
    Bumps `kotlinVersion` from 2.1.10 to 2.1.20.
    
    Updates `org.jetbrains.kotlin:kotlin-test-junit` from 2.1.10 to 2.1.20
    - [Release notes](https://github.com/JetBrains/kotlin/releases)
    - [Changelog](https://github.com/JetBrains/kotlin/blob/master/ChangeLog.md)
    - [Commits](https://github.com/JetBrains/kotlin/compare/v2.1.10...v2.1.20)
    
    Updates `jvm` from 2.1.10 to 2.1.20
    - [Release notes](https://github.com/JetBrains/kotlin/releases)
    - [Changelog](https://github.com/JetBrains/kotlin/blob/master/ChangeLog.md)
    - [Commits](https://github.com/JetBrains/kotlin/compare/v2.1.10...v2.1.20)
    
    Updates `org.jetbrains.kotlin.plugin.serialization` from 2.1.10 to 2.1.20
    - [Release notes](https://github.com/JetBrains/kotlin/releases)
    - [Changelog](https://github.com/JetBrains/kotlin/blob/master/ChangeLog.md)
    - [Commits](https://github.com/JetBrains/kotlin/compare/v2.1.10...v2.1.20)
    
    ---
    updated-dependencies:
    - dependency-name: org.jetbrains.kotlin:kotlin-test-junit
      dependency-type: direct:production
      update-type: version-update:semver-patch
    - dependency-name: jvm
      dependency-type: direct:production
      update-type: version-update:semver-patch
    - dependency-name: org.jetbrains.kotlin.plugin.serialization
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump com.zaxxer:HikariCP from 6.2.1 to 6.3.0 (#501)
    
    Bumps [com.zaxxer:HikariCP](https://github.com/brettwooldridge/HikariCP) from 6.2.1 to 6.3.0.
    - [Changelog](https://github.com/brettwooldridge/HikariCP/blob/dev/CHANGES)
    - [Commits](https://github.com/brettwooldridge/HikariCP/compare/HikariCP-6.2.1...HikariCP-6.3.0)
    
    ---
    updated-dependencies:
    - dependency-name: com.zaxxer:HikariCP
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Bump flywayVersion from 11.4.0 to 11.4.1
    
    Bumps `flywayVersion` from 11.4.0 to 11.4.1.
    
    Updates `org.flywaydb:flyway-core` from 11.4.0 to 11.4.1
    - [Release notes](https://github.com/flyway/flyway/releases)
    - [Commits](https://github.com/flyway/flyway/compare/flyway-11.4.0...flyway-11.4.1)
    
    Updates `org.flywaydb:flyway-database-postgresql` from 11.4.0 to 11.4.1
    
    ---
    updated-dependencies:
    - dependency-name: org.flywaydb:flyway-core
      dependency-type: direct:production
      update-type: version-update:semver-patch
    - dependency-name: org.flywaydb:flyway-database-postgresql
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump io.getunleash:unleash-client-java from 10.2.0 to 10.2.1
    
    Bumps [io.getunleash:unleash-client-java](https://github.com/Unleash/unleash-client-java) from 10.2.0 to 10.2.1.
    - [Release notes](https://github.com/Unleash/unleash-client-java/releases)
    - [Changelog](https://github.com/Unleash/unleash-client-java/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/Unleash/unleash-client-java/compare/v10.2.0...v10.2.1)
    
    ---
    updated-dependencies:
    - dependency-name: io.getunleash:unleash-client-java
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ group = "no.nav.amt-deltaker-bff"
 version = "1.0-SNAPSHOT"
 
 plugins {
-    val kotlinVersion = "2.1.10"
+    val kotlinVersion = "2.1.20"
 
     kotlin("jvm") version kotlinVersion
     id("io.ktor.plugin") version "3.1.1"
@@ -30,7 +30,7 @@ val logstashEncoderVersion = "8.0"
 val commonVersion = "3.2024.10.25_13.44-9db48a0dbe67"
 val poaoTilgangVersion = "2024.10.29_14.10-4fff597d6e1b"
 val kotestVersion = "5.9.1"
-val flywayVersion = "11.4.0"
+val flywayVersion = "11.4.1"
 val hikariVersion = "6.3.0"
 val kotliqueryVersion = "1.9.1"
 val postgresVersion = "42.7.5"
@@ -38,7 +38,7 @@ val caffeineVersion = "3.2.0"
 val mockkVersion = "1.13.17"
 val nimbusVersion = "10.0.2"
 val amtLibVersion = "1.2025.03.17_13.59-028cbd13def5"
-val unleashVersion = "10.2.0"
+val unleashVersion = "10.2.1"
 
 dependencies {
     implementation("io.ktor:ktor-server-content-negotiation-jvm")
