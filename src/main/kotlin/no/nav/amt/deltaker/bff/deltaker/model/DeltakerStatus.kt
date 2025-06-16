dac2c5c3bfba509f69c446ef3bb05700f2e1bfad
Izis Frøyhov <izis.froyhov@nav.no>
2025-06-16 09:21:20 +0200

    Utvider tolkning av "aktive statuser" for å rette feil med at de gamle deltakelsene ikke blir låst når man setter på venteliste og tildeler plass

--- src/main/kotlin/no/nav/amt/deltaker/bff/deltaker/model/DeltakerStatus.kt
+++ src/main/kotlin/no/nav/amt/deltaker/bff/deltaker/model/DeltakerStatus.kt
@@ -15,4 +15,6 @@ val AKTIVE_STATUSER = listOf(
     DeltakerStatus.Type.UTKAST_TIL_PAMELDING,
     DeltakerStatus.Type.VENTER_PA_OPPSTART,
     DeltakerStatus.Type.DELTAR,
+    DeltakerStatus.Type.SOKT_INN,
+    DeltakerStatus.Type.VENTELISTE,
 )
