23c41a0dbdadeded832f924ff65887a71b7f7d5a
Roar Skinderviken <roar.skinderviken@gmail.com>
2025-08-29 12:46:09 +0200

    Opprydding

--- src/main/kotlin/no/nav/amt/deltaker/bff/tiltakskoordinator/extensions/HendelseAnsvarligExtensions.kt
+++ src/main/kotlin/no/nav/amt/deltaker/bff/tiltakskoordinator/extensions/HendelseAnsvarligExtensions.kt
@@ -3,11 +3,8 @@ package no.nav.amt.deltaker.bff.tiltakskoordinator.extensions
 import no.nav.amt.deltaker.bff.tiltakskoordinator.ulesthendelse.model.AnsvarligNavnOgEnhet
 import no.nav.amt.lib.models.hendelse.HendelseAnsvarlig
 
-// TODO: Finne ut av hvor verdier skal hentes fra
 fun HendelseAnsvarlig.toAnsvarligNavnOgEnhet(): AnsvarligNavnOgEnhet = when (this) {
     is HendelseAnsvarlig.NavTiltakskoordinator -> AnsvarligNavnOgEnhet(navn, enhet.navn)
     is HendelseAnsvarlig.NavVeileder -> AnsvarligNavnOgEnhet(navn)
-    is HendelseAnsvarlig.Deltaker -> AnsvarligNavnOgEnhet()
-    is HendelseAnsvarlig.Arrangor -> AnsvarligNavnOgEnhet()
-    is HendelseAnsvarlig.System -> AnsvarligNavnOgEnhet()
+    else -> AnsvarligNavnOgEnhet()
 }
