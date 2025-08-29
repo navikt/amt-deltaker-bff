4cf9229cb2805021df3637dd2ca6e9e4173debec
robertkittilsen <robert.graarup.kittilsen@nav.no>
2025-08-29 09:57:38 +0200

    fjerner shadowjar, bruker Gradle installDist

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,3 @@
-import com.github.jengelman.gradle.plugins.shadow.transformers.PreserveFirstFoundResourceTransformer
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.gradle.api.tasks.testing.logging.TestLogEvent
 
@@ -12,7 +11,8 @@ plugins {
     id("io.ktor.plugin") version "3.2.3"
     id("org.jetbrains.kotlin.plugin.serialization") version kotlinVersion
     id("org.jlleitschuh.gradle.ktlint") version "13.1.0"
-    id("com.gradleup.shadow") version "9.0.2"
+    application
+    distribution
 }
 
 repositories {
@@ -128,13 +128,5 @@ tasks.jar {
         attributes(
             "Main-Class" to "no.nav.amt.deltaker.bff.ApplicationKt",
         )
-    }
-}
-
-tasks.shadowJar {
-    duplicatesStrategy = DuplicatesStrategy.INCLUDE
-    mergeServiceFiles()
-    transform<PreserveFirstFoundResourceTransformer> {
-        resources.add("logback.xml")
     }
 }
