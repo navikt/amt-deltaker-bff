7fdf99096dea135f0d4bba76cbaa4b3d261e9e93
Roar Skinderviken <roar.skinderviken@gmail.com>
2025-08-29 19:09:09 +0200

    Merge-fixes

--- src/test/kotlin/no/nav/amt/deltaker/bff/deltaker/db/DeltakerRepositoryTest.kt
+++ src/test/kotlin/no/nav/amt/deltaker/bff/deltaker/db/DeltakerRepositoryTest.kt
@@ -1,12 +1,24 @@
 package no.nav.amt.deltaker.bff.deltaker.db
 
+import io.kotest.assertions.assertSoftly
+import io.kotest.matchers.collections.shouldBeEmpty
 import io.kotest.matchers.collections.shouldHaveSize
+import io.kotest.matchers.collections.shouldNotBeEmpty
+import io.kotest.matchers.nulls.shouldBeNull
+import io.kotest.matchers.nulls.shouldNotBeNull
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNotBe
 import no.nav.amt.deltaker.bff.apiclients.DtoMappers.opprettKladdResponseFromDeltaker
 import no.nav.amt.deltaker.bff.deltaker.model.Deltaker
 import no.nav.amt.deltaker.bff.deltaker.model.Deltakeroppdatering
+import no.nav.amt.deltaker.bff.utils.DeltakerTestUtils.sammenlignDeltakere
 import no.nav.amt.deltaker.bff.utils.data.TestData
+import no.nav.amt.deltaker.bff.utils.data.TestData.lagDeltaker
+import no.nav.amt.deltaker.bff.utils.data.TestData.lagDeltakerEndring
+import no.nav.amt.deltaker.bff.utils.data.TestData.lagDeltakerKladd
+import no.nav.amt.deltaker.bff.utils.data.TestData.lagDeltakerStatus
+import no.nav.amt.deltaker.bff.utils.data.TestData.lagDeltakerliste
+import no.nav.amt.deltaker.bff.utils.data.TestData.leggTilHistorikk
 import no.nav.amt.deltaker.bff.utils.data.TestRepository
 import no.nav.amt.deltaker.bff.utils.data.endre
 import no.nav.amt.lib.models.arrangor.melding.Forslag
@@ -126,7 +138,7 @@ class DeltakerRepositoryTest {
             TestRepository.insert(deltaker.navBruker)
             TestRepository.insert(deltaker.deltakerliste)
 
-            val kladd = deltaker.toKladdResponse()
+            val kladd = opprettKladdResponseFromDeltaker(deltaker)
 
             deltakerRepository.create(kladd)
 
@@ -138,7 +150,7 @@ class DeltakerRepositoryTest {
             val deltaker = lagDeltakerKladd()
             TestRepository.insert(deltaker)
 
-            val kladd = deltaker.toKladdResponse()
+            val kladd = opprettKladdResponseFromDeltaker(deltaker)
 
             assertThrows(PSQLException::class.java) {
                 deltakerRepository.create(kladd)
