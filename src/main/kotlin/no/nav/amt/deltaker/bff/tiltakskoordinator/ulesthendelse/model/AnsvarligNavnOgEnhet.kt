0583cd432e65842abc419aa581048f33ecf64c07
Roar Skinderviken <roar.skinderviken@gmail.com>
2025-08-29 12:34:40 +0200

    Lagt til DTO-klassen AnsvarligNavnOgEnhet

---
+++ src/main/kotlin/no/nav/amt/deltaker/bff/tiltakskoordinator/ulesthendelse/model/AnsvarligNavnOgEnhet.kt
@@ -0,0 +1,6 @@
+package no.nav.amt.deltaker.bff.tiltakskoordinator.ulesthendelse.model
+
+data class AnsvarligNavnOgEnhet(
+    val endretAvNavn: String? = null,
+    val endretAvEnhet: String? = null,
+)
--- src/main/kotlin/no/nav/amt/deltaker/bff/tiltakskoordinator/ulesthendelse/model/UlestHendelse.kt
+++ src/main/kotlin/no/nav/amt/deltaker/bff/tiltakskoordinator/ulesthendelse/model/UlestHendelse.kt
@@ -1,6 +1,5 @@
 package no.nav.amt.deltaker.bff.tiltakskoordinator.ulesthendelse.model
 
-import no.nav.amt.lib.models.hendelse.HendelseAnsvarlig
 import java.time.LocalDateTime
 import java.util.UUID
 
@@ -8,6 +7,6 @@ data class UlestHendelse(
     val id: UUID,
     val opprettet: LocalDateTime,
     val deltakerId: UUID,
-    val ansvarlig: HendelseAnsvarlig, // TODO lage egen klasse for ansvarlig og?
+    val ansvarlig: AnsvarligNavnOgEnhet,
     val hendelse: UlestHendelseType,
 )
