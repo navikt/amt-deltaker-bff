b6f0b7b0af7d3218610c4f55dc353819e35be6f4
Roar Skinderviken <roar.skinderviken@gmail.com>
2025-08-29 12:39:29 +0200

    Bruker nå default-verdiene

--- src/main/kotlin/no/nav/amt/deltaker/bff/tiltakskoordinator/extensions/HendelseAnsvarligExtensions.kt
+++ src/main/kotlin/no/nav/amt/deltaker/bff/tiltakskoordinator/extensions/HendelseAnsvarligExtensions.kt
@@ -6,8 +6,8 @@ import no.nav.amt.lib.models.hendelse.HendelseAnsvarlig
 // TODO: Finne ut av hvor verdier skal hentes fra
 fun HendelseAnsvarlig.toAnsvarligNavnOgEnhet(): AnsvarligNavnOgEnhet = when (this) {
     is HendelseAnsvarlig.NavTiltakskoordinator -> AnsvarligNavnOgEnhet(navn, enhet.navn)
-    is HendelseAnsvarlig.NavVeileder -> AnsvarligNavnOgEnhet(navn, null)
-    is HendelseAnsvarlig.Deltaker -> AnsvarligNavnOgEnhet(null, null)
-    is HendelseAnsvarlig.Arrangor -> AnsvarligNavnOgEnhet(null, null)
-    is HendelseAnsvarlig.System -> AnsvarligNavnOgEnhet(null, null)
+    is HendelseAnsvarlig.NavVeileder -> AnsvarligNavnOgEnhet(navn)
+    is HendelseAnsvarlig.Deltaker -> AnsvarligNavnOgEnhet()
+    is HendelseAnsvarlig.Arrangor -> AnsvarligNavnOgEnhet()
+    is HendelseAnsvarlig.System -> AnsvarligNavnOgEnhet()
 }
