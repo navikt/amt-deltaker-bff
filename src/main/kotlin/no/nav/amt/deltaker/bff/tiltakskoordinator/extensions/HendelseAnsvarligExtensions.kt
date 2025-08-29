a5f91e28316736f4f5eaeb3597ee86fbbba886e1
Roar Skinderviken <roar.skinderviken@gmail.com>
2025-08-29 12:55:37 +0200

    AnsvarligNavnOgEnhet?

--- src/main/kotlin/no/nav/amt/deltaker/bff/tiltakskoordinator/extensions/HendelseAnsvarligExtensions.kt
+++ src/main/kotlin/no/nav/amt/deltaker/bff/tiltakskoordinator/extensions/HendelseAnsvarligExtensions.kt
@@ -3,8 +3,8 @@ package no.nav.amt.deltaker.bff.tiltakskoordinator.extensions
 import no.nav.amt.deltaker.bff.tiltakskoordinator.ulesthendelse.model.AnsvarligNavnOgEnhet
 import no.nav.amt.lib.models.hendelse.HendelseAnsvarlig
 
-fun HendelseAnsvarlig.toAnsvarligNavnOgEnhet(): AnsvarligNavnOgEnhet = when (this) {
-    is HendelseAnsvarlig.NavTiltakskoordinator -> AnsvarligNavnOgEnhet(navn, enhet.navn)
-    is HendelseAnsvarlig.NavVeileder -> AnsvarligNavnOgEnhet(navn)
-    else -> AnsvarligNavnOgEnhet()
+fun HendelseAnsvarlig.toAnsvarligNavnOgEnhet(): AnsvarligNavnOgEnhet? = when (this) {
+    is HendelseAnsvarlig.NavTiltakskoordinator -> AnsvarligNavnOgEnhet(endretAvNavn = navn, endretAvEnhet = enhet.navn)
+    is HendelseAnsvarlig.NavVeileder -> AnsvarligNavnOgEnhet(endretAvNavn = navn)
+    else -> null
 }
