c10c37d3b001dae0101af210a87c006b8b6aea70
Roar Skinderviken <roar.skinderviken@gmail.com>
2025-09-02 12:22:04 +0200

    Gjort test litt mer lesbar

--- src/test/kotlin/no/nav/amt/deltaker/bff/deltaker/api/model/AvsluttDeltakelseValideringTest.kt
+++ src/test/kotlin/no/nav/amt/deltaker/bff/deltaker/api/model/AvsluttDeltakelseValideringTest.kt
@@ -1,8 +1,10 @@
 package no.nav.amt.deltaker.bff.deltaker.api.model
 
-import io.kotest.assertions.throwables.shouldNotThrow
+import io.kotest.assertions.throwables.shouldNotThrowAny
 import io.kotest.assertions.throwables.shouldThrow
-import no.nav.amt.deltaker.bff.utils.data.TestData
+import io.kotest.matchers.shouldBe
+import no.nav.amt.deltaker.bff.utils.data.TestData.lagDeltaker
+import no.nav.amt.deltaker.bff.utils.data.TestData.lagDeltakerStatus
 import no.nav.amt.lib.models.deltaker.DeltakerEndring
 import no.nav.amt.lib.models.deltaker.DeltakerStatus
 import org.junit.jupiter.api.Test
@@ -12,64 +14,52 @@ import java.util.UUID
 class AvsluttDeltakelseValideringTest {
     @Test
     fun `valider - deltaker er uendret - feiler`() {
-        shouldThrow<IllegalArgumentException> {
-            val deltaker = TestData.lagDeltaker(
-                status = TestData.lagDeltakerStatus(
-                    type = DeltakerStatus.Type.HAR_SLUTTET,
-                    aarsak = DeltakerStatus.Aarsak.Type.FATT_JOBB,
-                ),
-                sluttdato = LocalDate.now().minusDays(1),
-            )
-            val request = AvsluttDeltakelseRequest(
-                aarsak = DeltakerEndring.Aarsak(DeltakerEndring.Aarsak.Type.FATT_JOBB, null),
-                sluttdato = LocalDate.now().minusDays(1),
-                begrunnelse = "Begrunnelse",
-                forslagId = UUID.randomUUID(),
-            )
-
-            request.valider(deltaker)
+        val thrown = shouldThrow<IllegalArgumentException> {
+            requestInTest.valider(deltakerInTest)
         }
+
+        thrown.message shouldBe "Kan ikke avslutte deltakelse med uendret sluttdato og årsak"
     }
 
     @Test
     fun `valider - deltaker har annen sluttdato - ok`() {
-        shouldNotThrow<IllegalArgumentException> {
-            val deltaker = TestData.lagDeltaker(
-                status = TestData.lagDeltakerStatus(
-                    type = DeltakerStatus.Type.HAR_SLUTTET,
-                    aarsak = DeltakerStatus.Aarsak.Type.FATT_JOBB,
-                ),
-                sluttdato = LocalDate.now().minusDays(2),
-            )
-            val request = AvsluttDeltakelseRequest(
-                aarsak = DeltakerEndring.Aarsak(DeltakerEndring.Aarsak.Type.FATT_JOBB, null),
-                sluttdato = LocalDate.now().minusDays(1),
-                begrunnelse = "Begrunnelse",
-                forslagId = UUID.randomUUID(),
-            )
+        val deltaker = deltakerInTest.copy(sluttdato = yesterday.minusDays(1))
 
-            request.valider(deltaker)
+        shouldNotThrowAny {
+            requestInTest.valider(deltaker)
         }
     }
 
     @Test
     fun `valider - deltaker har annen sluttaarsak - ok`() {
-        shouldNotThrow<IllegalArgumentException> {
-            val deltaker = TestData.lagDeltaker(
-                status = TestData.lagDeltakerStatus(
-                    type = DeltakerStatus.Type.HAR_SLUTTET,
-                    aarsak = DeltakerStatus.Aarsak.Type.FATT_JOBB,
-                ),
-                sluttdato = LocalDate.now().minusDays(2),
-            )
-            val request = AvsluttDeltakelseRequest(
-                aarsak = DeltakerEndring.Aarsak(DeltakerEndring.Aarsak.Type.IKKE_MOTT, null),
-                sluttdato = LocalDate.now().minusDays(2),
-                begrunnelse = "Begrunnelse",
-                forslagId = UUID.randomUUID(),
-            )
+        val deltaker = deltakerInTest.copy(sluttdato = yesterday.minusDays(1))
 
+        val request = requestInTest.copy(
+            aarsak = DeltakerEndring.Aarsak(DeltakerEndring.Aarsak.Type.IKKE_MOTT, null),
+            sluttdato = yesterday.minusDays(1),
+        )
+
+        shouldNotThrowAny {
             request.valider(deltaker)
         }
+    }
+
+    companion object {
+        private val yesterday: LocalDate = LocalDate.now().minusDays(1)
+
+        private val deltakerInTest = lagDeltaker(
+            status = lagDeltakerStatus(
+                type = DeltakerStatus.Type.HAR_SLUTTET,
+                aarsak = DeltakerStatus.Aarsak.Type.FATT_JOBB,
+            ),
+            sluttdato = yesterday,
+        )
+
+        private val requestInTest = AvsluttDeltakelseRequest(
+            aarsak = DeltakerEndring.Aarsak(DeltakerEndring.Aarsak.Type.FATT_JOBB, null),
+            sluttdato = yesterday,
+            begrunnelse = "Begrunnelse",
+            forslagId = UUID.randomUUID(),
+        )
     }
 }
