bb0c56b7d238774b6a53656828785aec6c84e052
MartheFH <136322563+MartheFH@users.noreply.github.com>
2025-10-13 10:55:16 +0200

    Sende med oppstartstypen for deltakerlister til tiltakskoordinatorflaten (#732)

--- src/main/kotlin/no/nav/amt/deltaker/bff/tiltakskoordinator/api/response/DeltakerlisteResponse.kt
+++ src/main/kotlin/no/nav/amt/deltaker/bff/tiltakskoordinator/api/response/DeltakerlisteResponse.kt
@@ -1,6 +1,7 @@
 package no.nav.amt.deltaker.bff.tiltakskoordinator.api.response
 
 import no.nav.amt.deltaker.bff.tiltakskoordinator.model.Tiltakskoordinator
+import no.nav.amt.lib.models.deltakerliste.Oppstartstype
 import no.nav.amt.lib.models.deltakerliste.tiltakstype.Tiltakskode
 import java.time.LocalDate
 import java.util.UUID
@@ -11,6 +12,7 @@ data class DeltakerlisteResponse(
     val tiltakskode: Tiltakskode,
     val startdato: LocalDate,
     val sluttdato: LocalDate?,
+    val oppstartstype: Oppstartstype,
     val apentForPamelding: Boolean,
     val antallPlasser: Int,
     val koordinatorer: List<Tiltakskoordinator>,
