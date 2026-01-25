8cad9b69f6c77ca74156eac9fafabc580c8e5551
Roar Skinderviken <roar.skinderviken@gmail.com>
2026-01-25 10:51:23 +0100

    Deploy

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,6 @@ version = "1.0-SNAPSHOT"
 
 plugins {
     val kotlinVersion = "2.2.21"
-
     kotlin("jvm") version kotlinVersion
     id("io.ktor.plugin") version "3.3.3"
     id("org.jetbrains.kotlin.plugin.serialization") version kotlinVersion
